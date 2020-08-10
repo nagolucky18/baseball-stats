2e482467ab2615e1b5dd4e55faba3f90935295f8
enag2000 <enag2000@gmail.com>
2020-08-10 15:26:07 -0700

    get_stat, get_stats, and print_stats implemented

--- baseball_stats.py
+++ baseball_stats.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 from player import Player
 
 # USE KEYWORD ARGS FOR sort_batters and sort pitchers!
-def sort_batters(reader, metric, n=float('inf'), year=2019, min_ab=0):
+def sort_batters(metric, n=float('inf'), year=2019, min_ab=0):
     """
     Sort a given number of batters in a given year by a provided metric. 
     If no number of players is provided, sort all players in the 
@@ -16,7 +16,7 @@ def sort_batters(reader, metric, n=float('inf'), year=2019, min_ab=0):
     relevant metric.
     """
     
-def sort_pitchers(reader, metric, n=float('inf'), year=2019, min_ip=0):
+def sort_pitchers(metric, n=float('inf'), year=2019, min_ip=0):
     """
     Sort a given number of pitchers in a given year by a provided 
     metric. If no number of players is provided, sort all players in the 
@@ -27,34 +27,87 @@ def sort_pitchers(reader, metric, n=float('inf'), year=2019, min_ip=0):
     """
 
 # Represent player name as tuple (last, first)
-def get_stat(reader, year, player_name, stat):
+def get_stat(year, player_name, player_type, stat):
     """Get a certain stat based on the arguments provided."""
-    if not name_in_dataset(player_name):
+    if not name_in_dataset(player_name, player_type):
         raise ValueError
         
-    return 
-def get_stats(reader, year, player_name):
-    """Get stats based on the arguments provided."""
-    if not name_in_dataset(player_name):
-        raise ValueError
-
-
+    data = get_path(player_type)
+        
+    with open(data) as f:
+        reader = csv.DictReader(f)
+        
+        lookup_name = player_name[0][0:5] + player_name[1][0:2] + '01'
+        for row in reader:
+            if row['playerID'] == lookup_name and row['yearID'] == year:
+                    return {stat: row[stat]}
     
+def get_stats(year, player_name, player_type):
+    """Get stats based on the arguments provided."""
+    if not name_in_dataset(player_name, player_type):
+        raise ValueError
+        
+    data = get_path(player_type)
+    
+    with open(data) as f:
+        reader = csv.DictReader(f)
+        
+        lookup_name = player_name[0][0:5] + player_name[1][0:2] + '01'
+        for row in reader:
+            if row['playerID'] == lookup_name and row['yearID'] == year:
+                    return row
+                    
+def print_stats(player_name, stats):
+    """
+    Print the stats to the terminal. Assumes stat argument is a valid
+    dictionary containing stats about the given player.
+    """
+    # TODO - title case player name  
+    if len(stats) == 1:
+        for key in stats.keys():
+            print('{0} stat for {1}, {2}: {3}'.format(key, 
+            player_name[0].title(), player_name[1].title(), stats[key]))
+    else:
+        print('Stats for {0}, {1}:'.format(player_name[0].title(), 
+            player_name[1].title()))
+        for key in stats.keys():
+            print('{0}: {1}'.format(key, stats[key]))
+                
 def plot_stats(players):
     """Plot stats based on the arguments provided."""
     
-def name_in_dataset(player_name):
+def name_in_dataset(player_name, player_type):
     """Return true if the player is in the data set and false otherwise."""
-    # TO BE IMPLEMENTED
+    data = get_path(player_type)
     
-
+    with open(data) as f:
+        reader = csv.DictReader(f)
+        
+        lookup_name = player_name[0][0:5] + player_name[1][0:2] + '01'
+        for row in reader:
+            if row['playerID'] == lookup_name:
+                return True
+        return False
+        
+def get_path(query):
+    """Return the requested file based on the query."""
+    # Support for other data sets to be implemented at later date.
+    if query == 'bat':
+        ds = os.path.dirname(__file__) + '\\data\\core\\Batting.csv'
+    elif query == 'pit':
+        ds = os.path.dirname(__file__) + '\\data\\core\\Pitching.csv'
+    else:
+        raise ValueError
+    
+    return ds
+    
 def main():
     """Main for baseball_stats."""
-    fp = os.path.dirname(__file__) + '\\data\\core\\Batting.csv'
-    with open(fp) as f:
-        r = csv.DictReader(f)
-        for row in r:
-            print(1)
+    pn = ('myers', 'wil')
+    print_stats(pn, (get_stats('2015', pn, 'bat')))
+    
+    pn_2 = ('ohtani', 'shohei')
+    print_stats(pn_2, (get_stat('2018', pn_2, 'pit', 'SO')))
     
 if __name__ == '__main__':
     main()
